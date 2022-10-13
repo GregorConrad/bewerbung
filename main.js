97b7084db495c267f5f17841e3473a6f5b44bd09
gregorconrad <gconrad2006@gmcil.com>
2022-10-13 09:46:44 +0200

    code dokumentiert

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+// holt sich Verbindung zu den Klassen die in den Klammern stehen aus kontakt.html
 const form = document.querySelector(".result");
 const p = document.querySelector(".send");
 const button = document.querySelector(".button");
 
+// Erkennt, dass Button geklickt wird und macht "Ihre Email wurde gesendet" schwarz
 button.addEventListener("click", e => {
     p.style.cssText="color: black";
+    // Kontaktformular wird zurückgesetzt 
     form.reset();
-    
+        
+    // "Ihre Email wurde gesendet" wird nach Zeit: X wieder weiß 
     const timer = setTimeout(() => { 
         p.style.cssText="color: white";   
     }, 4000);
